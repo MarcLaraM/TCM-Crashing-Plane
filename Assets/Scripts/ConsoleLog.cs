50a02429c94e494f99ed5b40ebd0f59b8f45cb8c
MarcLaraM <mlaram@edu.tecnocampus.cat>
2025-04-01 18:07:54 +0200

    coin

---
+++ Assets/Scripts/ConsoleLog.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ConsoleLock : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start");
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+
+    private void On
+}
